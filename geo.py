fe65b51ad0add643f4f4f2a5a50080268d11bc9d
Pius Fischer <pius@nightjuggler.com>
2017-02-09 14:12:30 -0800

    * Added the AlbersSphere class for Albers projections for spheres * Renamed the AlbersProjection class to AlbersEllipsoid and allow it to handle the case when the ellipsoid is a sphere, i.e. when the flattening (self.spheroid.f) is 0 (and the eccentricity is also 0)

--- geo.py
+++ geo.py
@@ -6,7 +6,11 @@ radiansPerDegree = 0.0174532925199433 # round(math.pi / 180.0, 16)
 class Spheroid(object):
 	def __init__(self, equatorialRadius, inverseFlattening):
 		self.a = equatorialRadius
-		self.f = 1 / inverseFlattening
+
+		if inverseFlattening == 0 or inverseFlattening is None:
+			self.f = 0.0
+		else:
+			self.f = 1.0 / inverseFlattening
 
 		# a = equatorial radius (aka semi-major axis)
 		# c = polar radius (aka semi-minor axis)
@@ -21,7 +25,7 @@ class Spheroid(object):
 Clarke_1866_Ellipsoid = Spheroid(6378206.4, 294.9786982) # See https://en.wikipedia.org/wiki/North_American_Datum
 GRS_1980_Ellipsoid = Spheroid(6378137.0, 298.257222101) # See https://en.wikipedia.org/wiki/GRS_80
 
-class AlbersProjection(object):
+class AlbersEllipsoid(object):
 	#
 	# Formulas according to pages 101-102 of "Map Projections - A Working Manual"
 	# by John P. Snyder, 1987 (U.S. Geological Survey Professional Paper 1395)
@@ -43,6 +47,9 @@ class AlbersProjection(object):
 		self.C = m1*m1 + self.n*q1
 		self.p0 = self.spheroid.a * math.sqrt(self.C - self.n*q0) / self.n
 
+		if self.spheroid.f == 0:
+			self.p0 *= 2
+
 		self.falseEasting = 0.0
 		self.falseNorthing = 0.0
 
@@ -55,12 +62,19 @@ class AlbersProjection(object):
 	def genMQ(self, latitude):
 		latitude *= radiansPerDegree
 		sinlat = math.sin(latitude)
+		coslat = math.cos(latitude)
+
+		if self.spheroid.f == 0:
+			return coslat, sinlat
+
 		e = self.spheroid.e
 		ee = self.spheroid.ee
 		es = e * sinlat
 		eess = 1 - ee * sinlat * sinlat
-		m = math.cos(latitude) / math.sqrt(eess)
-		q = (1 - ee) * (sinlat / eess - 1/(2*e) * math.log((1 - es) / (1 + es)))
+
+		m = coslat / math.sqrt(eess)
+		q = (1 - ee) * (sinlat / eess - math.log((1 - es) / (1 + es)) / (2*e))
+
 		return m, q
 
 	def project(self, longitude, latitude):
@@ -68,6 +82,10 @@ class AlbersProjection(object):
 
 		theta = self.n * (longitude - self.originLongitude) * radiansPerDegree
 		p = self.spheroid.a * math.sqrt(self.C - self.n*q) / self.n
+
+		if self.spheroid.f == 0:
+			theta /= 2
+			p *= 2
 
 		x = p * math.sin(theta)
 		y = self.p0 - p * math.cos(theta)
@@ -79,6 +97,8 @@ class AlbersProjection(object):
 		e = self.spheroid.e
 		ee = self.spheroid.ee
 		n = self.n
+		if self.spheroid.f == 0:
+			n /= 2
 
 		x -= self.falseEasting
 		y -= self.falseNorthing
@@ -86,16 +106,18 @@ class AlbersProjection(object):
 
 		theta = math.atan2(x, y)
 		p = math.hypot(x, y)
+		q = (self.C - p*p*n*n/(a*a)) / n
 
 		longitude = self.originLongitude + theta / n / radiansPerDegree
+		latitude = math.asin(q / 2.0)
 
-		q = (self.C - p*p*n*n/(a*a)) / n
+		if self.spheroid.f == 0:
+			return longitude, latitude / radiansPerDegree
+
 		qlat90 = 1 - (1 - ee)/(2*e) * math.log((1 - e) / (1 + e))
 
 		if round(abs(q), 12) == round(qlat90, 12):
 			return longitude, math.copysign(90, q)
-
-		latitude = math.asin(q / 2)
 
 		while True:
 			sinlat = math.sin(latitude)
@@ -103,13 +125,65 @@ class AlbersProjection(object):
 			eess = 1 - ee * sinlat * sinlat
 
 			d1 = eess * eess / (2 * math.cos(latitude))
-			d2 = q / (1 - ee) - sinlat / eess + 1/(2*e) * math.log((1 - es) / (1 + es))
+			d2 = q / (1 - ee) - sinlat / eess + math.log((1 - es) / (1 + es)) / (2*e)
 
 			delta = d1 * d2
 			latitude += delta
 
 			if abs(delta / radiansPerDegree) < 0.00000001:
 				break
+
+		return longitude, latitude / radiansPerDegree
+
+class AlbersSphere(object):
+	def __init__(self, lat0, lng0, lat1, lat2, R):
+		self.originLatitude = lat0
+		self.originLongitude = lng0
+		self.standardParallel1 = lat1
+		self.standardParallel2 = lat2
+		self.R = R
+
+		s0 = math.sin(lat0 * radiansPerDegree)
+		s1 = math.sin(lat1 * radiansPerDegree)
+		s2 = math.sin(lat2 * radiansPerDegree)
+		c1 = math.cos(lat1 * radiansPerDegree)
+
+		self.n = (s1 + s2) / 2.0
+		self.C = c1*c1 + 2*self.n*s1
+		self.p0 = R * math.sqrt(self.C - 2*self.n*s0) / self.n
+
+		self.falseEasting = 0.0
+		self.falseNorthing = 0.0
+
+	def setFalseEasting(self, falseEasting):
+		self.falseEasting = falseEasting
+
+	def setFalseNorthing(self, falseNorthing):
+		self.falseNorthing = falseNorthing
+
+	def project(self, longitude, latitude):
+		s = math.sin(latitude * radiansPerDegree)
+
+		theta = self.n * (longitude - self.originLongitude) * radiansPerDegree
+		p = self.R * math.sqrt(self.C - 2*self.n*s) / self.n
+
+		x = p * math.sin(theta)
+		y = self.p0 - p * math.cos(theta)
+
+		return x + self.falseEasting, y + self.falseNorthing
+
+	def inverse(self, x, y):
+		x -= self.falseEasting
+		y -= self.falseNorthing
+		y = self.p0 - y
+		n = self.n
+
+		theta = math.atan2(x, y)
+		p = math.hypot(x, y)
+		pnR = p * n / self.R
+
+		longitude = self.originLongitude + theta / n / radiansPerDegree
+		latitude = math.asin((self.C - pnR*pnR) / (2*n))
 
 		return longitude, latitude / radiansPerDegree
 
@@ -141,13 +215,28 @@ def check(projection, input, expectedOutput, roundDigits=4, inverse=False):
 
 def test1():
 	#
+	# Numerical Example from page 291 of "Map Projections"
+	#
+	lnglat = (-75, 35)
+	xy = (0.2952720, 0.2416774)
+
+	projection = AlbersSphere(23.0, -96.0, 29.5, 45.5, 1)
+	output = check(projection, lnglat, xy, roundDigits=7)
+	check(projection, output, lnglat, roundDigits=0, inverse=True)
+
+	projection = AlbersEllipsoid(23.0, -96.0, 29.5, 45.5, Spheroid(1, 0))
+	output = check(projection, lnglat, xy, roundDigits=7)
+	check(projection, output, lnglat, roundDigits=0, inverse=True)
+
+	#
 	# Numerical Example from page 292 of "Map Projections"
 	#
-	projection = AlbersProjection(23.0, -96.0, 29.5, 45.5, Clarke_1866_Ellipsoid)
+	projection = AlbersEllipsoid(23.0, -96.0, 29.5, 45.5, Clarke_1866_Ellipsoid)
 
 	lnglat = (-75, 35)
 	xy = (1885472.7, 1535925.0)
-	check(projection, lnglat, xy, roundDigits=1)
+	xy = check(projection, lnglat, xy, roundDigits=1)
+	check(projection, xy, lnglat, roundDigits=0, inverse=True)
 
 	#
 	# The following two tests check inverse projection when the latitude is +90 or -90 degrees
@@ -169,7 +258,7 @@ def test2():
 	# The coordinate system used is EPSG:3310 (see http://epsg.io/3310)
 	# aka NAD83 / California Albers
 	#
-	projection = AlbersProjection(0.0, -120.0, 34.0, 40.5, GRS_1980_Ellipsoid)
+	projection = AlbersEllipsoid(0.0, -120.0, 34.0, 40.5, GRS_1980_Ellipsoid)
 	projection.setFalseNorthing(-4000000.0)
 
 	xy = (140545.134, -71493.1984)
