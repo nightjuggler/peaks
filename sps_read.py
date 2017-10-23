8ac15c151c1d6bb54a536fd0f5c6ce8a7446cb71
Pius Fischer <pius@nightjuggler.com>
2017-10-23 15:16:51 -0700

    Support centimeter precision for the NAVD 88 elevation in an NGS datasheet (see, for example, Granite Chief)

--- sps_read.py
+++ sps_read.py
@@ -303,7 +303,7 @@ def printLandManagementAreas():
 ngsLinkPrefix = 'https://www.ngs.noaa.gov/cgi-bin/ds_mark.prl?PidBox='
 topoLinkPrefix = 'https://ngmdb.usgs.gov/img4/ht_icons/Browse/'
 topoLinkPattern = re.compile('^([A-Z][A-Z])/(\\1_[A-Z][a-z]+(?:%20[A-Z][a-z]+)*)_[0-9]{6}_([0-9]{4})_([0-9]{5})\\.jpg$')
-elevFromNGS = re.compile('^([0-9]{4}(?:\\.[0-9])?)m \\(NAVD 88\\) NGS Data Sheet &quot;[A-Z][a-z]+(?: [A-Z][a-z]+)*(?: [0-9]+)?&quot; \\(([A-Z]{2}[0-9]{4})\\)$')
+elevFromNGS = re.compile('^([0-9]{4}(?:\\.[0-9]{1,2})?)m \\(NAVD 88\\) NGS Data Sheet &quot;[A-Z][a-z]+(?: [A-Z][a-z]+)*(?: [0-9]+)?&quot; \\(([A-Z]{2}[0-9]{4})\\)$')
 elevFromTopo = re.compile('^((?:[0-9]{4}(?:(?:\\.[0-9])|(?:-[0-9]{4}))?m)|(?:[0-9]{4,5}(?:-[0-9]{4,5})?\')) \\(NGVD 29\\) USGS (7\\.5|15)\' Quad \\(1:([0-9]{2}),([0-9]{3})\\) &quot;([\\. A-Za-z]+), ([A-Z][A-Z])&quot; \\(([0-9]{4})(?:/[0-9]{4})?\\)$')
 contourIntervals = (10, 20, 40, 80)
 
