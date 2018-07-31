468a9d2640be0322256f5e8f9f5f453391994f13
Pius Fischer <pius@nightjuggler.com>
2018-07-31 01:06:02 -0700

    Display outline of wilderness area when click occurs inside a wilderness area.

--- MapServer.js
+++ MapServer.js
@@ -5,11 +5,11 @@ function addWildernessTileLayer(map)
 {
 	"use strict";
 
-	var earthRadius = 6378137.0; // WGS 84 equatorial radius in meters
+	var earthRadius = 6378137; // WGS 84 equatorial radius in meters
 	var earthCircumference = 2 * Math.PI * earthRadius;
 	var tileOrigin = -(Math.PI * earthRadius);
 
-	var WildernessServer = 'https://gisservices.cfc.umt.edu/arcgis/rest/services/ProtectedAreas/National_Wilderness_Preservation_System/MapServer';
+	var wildernessMapServer = 'https://gisservices.cfc.umt.edu/arcgis/rest/services/ProtectedAreas/National_Wilderness_Preservation_System/MapServer';
 
 	var WildernessTileLayer = L.GridLayer.extend({
 	createTile: function(tileCoords, done)
@@ -20,7 +20,7 @@ function addWildernessTileLayer(map)
 
 		var tileSize = this.getTileSize();
 
-		var url = WildernessServer + '/export?' + [
+		var url = wildernessMapServer + '/export?' + [
 			'bbox=' + [x.toFixed(4), (-y-m).toFixed(4), (x+m).toFixed(4), (-y).toFixed(4)].join(','),
 			'layers=0',
 			'size=' + tileSize.x + ',' + tileSize.y,
@@ -38,7 +38,7 @@ function addWildernessTileLayer(map)
 	}
 	});
 
-	map.addLayer(new WildernessTileLayer({opacity: 0.5}));
+	map.addLayer(new WildernessTileLayer({opacity: 0.5, zIndex: 210}));
 
 	var wildernessLink = document.createElement('a');
 	var wildernessName = document.createTextNode('');
@@ -53,6 +53,15 @@ function addWildernessTileLayer(map)
 	popupDiv.appendChild(wildernessYear);
 	var popup = L.popup({maxWidth: 600}).setContent(popupDiv);
 
+	var outline = null;
+	var outlineCache = {};
+	var outlineColor = {
+		BLM: '#0000FF', // Blue   (fill color is #FFFF00)
+		FWS: '#FFA500', // Orange (fill color is #FFAA00)
+		NPS: '#800080', // Purple (fill color is #A900E6)
+		USFS:'#008000', // Green  (fill color is #38A800)
+	};
+
 	map.on('click', function(event) {
 		var size = map.getSize();
 		var bounds = map.getBounds();
@@ -60,7 +69,7 @@ function addWildernessTileLayer(map)
 		var ne = bounds.getNorthEast();
 		var ll = event.latlng;
 
-		var url = WildernessServer + '/identify?' + [
+		var url = wildernessMapServer + '/identify?' + [
 			'geometry=' + ll.lng.toFixed(6) + ',' + ll.lat.toFixed(6),
 			'geometryType=esriGeometryPoint',
 			'sr=4326',
@@ -69,22 +78,38 @@ function addWildernessTileLayer(map)
 				sw.lng.toFixed(6), sw.lat.toFixed(6),
 				ne.lng.toFixed(6), ne.lat.toFixed(6)].join(','),
 			'imageDisplay=' + size.x + ',' + size.y + ',96',
-			'returnGeometry=false',
-			'geometryPrecision=6',
+			'returnGeometry=true',
+			'geometryPrecision=5',
 			'returnUnformattedValues=false',
 			'returnFieldName=false',
 			'f=json'].join('&');
 
 		loadJSON(url, function(json) {
+			if (outline) { outline.remove(); outline = null; }
 			if (json.results.length === 0) return;
 
-			var attr = json.results[0].attributes;
+			json = json.results[0];
+			var attr = json.attributes;
+			var agency = attr.Agency;
+			if (agency === 'FS') agency = 'USFS';
 
 			wildernessLink.href = attr.URL;
 			wildernessName.nodeValue = attr.NAME;
-			wildernessYear.nodeValue = ' (' + attr.Agency + ') (' + attr.YearDesignated + ')';
+			wildernessYear.nodeValue = '(' + agency + ') (' + attr.YearDesignated + ')';
 
 			map.openPopup(popup.setLatLng(ll));
+
+			if (json.geometryType !== 'esriGeometryPolygon') return;
+
+			var objectId = attr.OBJECTID_1;
+
+			outline = outlineCache[objectId];
+			if (!outline)
+				outlineCache[objectId] = outline = L.GeoJSON.geometryToLayer(
+					{type: 'Polygon', coordinates: json.geometry.rings},
+					{color: outlineColor[agency] || '#000000', fillOpacity: 0});
+
+			outline.addTo(map);
 		});
 	});
 }
