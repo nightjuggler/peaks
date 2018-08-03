4d444d4e68ac4cf999dbb0fafbd9d5cf9eec3a47
Pius Fischer <pius@nightjuggler.com>
2018-08-03 03:05:27 -0700

    First iteration of more generalized code that will allow adding/removing tile layers from different map servers and querying multiple map servers with a single click.

--- MapServer.js
+++ MapServer.js
@@ -1,45 +1,100 @@
 /* globals document, Image, L, loadJSON */
-/* exported addWildernessTileLayer */
+/* exported MapServer */
 
-function addWildernessTileLayer(map)
+var MapServer = (function() {
+'use strict';
+
+var MapServer = {
+	wildernessSpec: {
+		// Spatial Reference: 102113 (3785)
+		alias: 'w',
+		url: 'https://gisservices.cfc.umt.edu/arcgis/rest/services/ProtectedAreas/National_Wilderness_Preservation_System/MapServer',
+		exportLayers: '0',
+		transparent: 'true',
+		options: {opacity: 0.5, zIndex: 210},
+		queryLayer: '0',
+		queryFields: ['OBJECTID_1', 'NAME', 'URL', 'Agency', 'YearDesignated'],
+	},
+	topoSpec: {
+		// Spatial Reference: 102100 (3857)
+		alias: 't',
+		url: 'https://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer',
+		exportLayers: '0',
+		transparent: 'false',
+		options: {zIndex: 210},
+	},
+	imageryTopoSpec: {
+		// Spatial Reference: 102100 (3857)
+		alias: 'it',
+		url: 'https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryTopo/MapServer',
+		exportLayers: '0',
+		transparent: 'false',
+		options: {zIndex: 210},
+	},
+	stateCountySpec: {
+		// Spatial Reference: 102100 (3857)
+		alias: 'sc',
+		url: 'https://tigerweb.geo.census.gov/arcgis/rest/services/TIGERweb/State_County/MapServer',
+		exportLayers: '0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16', // states and counties
+		transparent: 'true',
+		options: {zIndex: 210},
+		queryLayer: '12',
+		queryFields: ['OBJECTID', 'NAME', 'STUSAB'],
+	},
+	countySpec: {
+		// Spatial Reference: 102100 (3857)
+		alias: 'c',
+		url: 'https://tigerweb.geo.census.gov/arcgis/rest/services/TIGERweb/State_County/MapServer',
+		exportLayers: '1,3,5,7,9,11,13', // counties only
+		transparent: 'true',
+		options: {zIndex: 210},
+		queryLayer: '13',
+		queryFields: ['OBJECTID', 'NAME'],
+	},
+};
+
+var earthRadius = 6378137; // WGS 84 equatorial radius in meters
+var earthCircumference = 2 * Math.PI * earthRadius;
+var tileOrigin = -(Math.PI * earthRadius);
+
+function tileLayer(spec)
 {
-	"use strict";
-
-	var earthRadius = 6378137; // WGS 84 equatorial radius in meters
-	var earthCircumference = 2 * Math.PI * earthRadius;
-	var tileOrigin = -(Math.PI * earthRadius);
-
-	var wildernessMapServer = 'https://gisservices.cfc.umt.edu/arcgis/rest/services/ProtectedAreas/National_Wilderness_Preservation_System/MapServer';
-
-	var WildernessTileLayer = L.GridLayer.extend({
+	return L.GridLayer.extend({
 	createTile: function(tileCoords, done)
 	{
 		var m = earthCircumference / (1 << tileCoords.z); // tile size in meters
 		var x = tileOrigin + m*tileCoords.x;
 		var y = tileOrigin + m*tileCoords.y;
+		var p = 2;
 
 		var tileSize = this.getTileSize();
 
-		var url = wildernessMapServer + '/export?' + [
-			'bbox=' + [x.toFixed(4), (-y-m).toFixed(4), (x+m).toFixed(4), (-y).toFixed(4)].join(','),
-			'layers=0',
+		var url = spec.url + '/export?' + [
+			'bbox=' + [x.toFixed(p), (-y-m).toFixed(p), (x+m).toFixed(p), (-y).toFixed(p)].join(','),
+			'bboxSR=102113',
+			'layers=show:' + spec.exportLayers,
 			'size=' + tileSize.x + ',' + tileSize.y,
+			'imageSR=102113',
 			'format=png',
-			'transparent=true',
+			'transparent=' + spec.transparent,
 			'f=image'].join('&');
 
 		var tile = new Image(tileSize.x, tileSize.y);
 
 		tile.addEventListener('load', function() { done(null, tile); }, false);
-		tile.addEventListener('error', function() { done("Failed to load " + url, tile); }, false);
+		tile.addEventListener('error', function() { done('Failed to load ' + url, tile); }, false);
 		tile.src = url;
 
 		return tile;
 	}
 	});
-
-	map.addLayer(new WildernessTileLayer({opacity: 0.5, zIndex: 210}));
-
+}
+MapServer.addLayer = function(map, spec)
+{
+	map.addLayer(new (tileLayer(spec))(spec.options));
+};
+MapServer.enableQuery = function(map)
+{
 	var wildernessLink = document.createElement('a');
 	var wildernessName = document.createTextNode('');
 	var wildernessYear = document.createTextNode('');
@@ -61,55 +116,75 @@ function addWildernessTileLayer(map)
 		NPS: '#800080', // Purple (fill color is #A900E6)
 		USFS:'#008000', // Green  (fill color is #38A800)
 	};
+	var geojson = false;
+
+	var spec = MapServer.wildernessSpec;
+
+	var baseURL = spec.url + '/' + spec.queryLayer + '/query?f=' + (geojson ? 'geojson' : 'json');
+	var queryLL = [baseURL,
+		'returnGeometry=false',
+		'outFields=' + spec.queryFields.join(','),
+		'spatialRel=esriSpatialRelIntersects',
+		'inSR=4326', // WGS 84 (EPSG:4326) longitude/latitude
+		'geometryType=esriGeometryPoint',
+		'geometry='].join('&');
+	var queryID = [baseURL,
+		'returnGeometry=true',
+		'geometryPrecision=5',
+		'outSR=4326',
+		'objectIds='].join('&');
+
+	function removeOutline()
+	{
+		if (outline) { outline.remove(); outline = null; }
+	}
 
 	map.on('click', function(event) {
-		var size = map.getSize();
-		var bounds = map.getBounds();
-		var sw = bounds.getSouthWest();
-		var ne = bounds.getNorthEast();
-		var ll = event.latlng;
 
-		var url = wildernessMapServer + '/identify?' + [
-			'geometry=' + ll.lng.toFixed(6) + ',' + ll.lat.toFixed(6),
-			'geometryType=esriGeometryPoint',
-			'sr=4326',
-			'tolerance=4',
-			'mapExtent=' + [
-				sw.lng.toFixed(6), sw.lat.toFixed(6),
-				ne.lng.toFixed(6), ne.lat.toFixed(6)].join(','),
-			'imageDisplay=' + size.x + ',' + size.y + ',96',
-			'returnGeometry=true',
-			'geometryPrecision=5',
-			'returnUnformattedValues=false',
-			'returnFieldName=false',
-			'f=json'].join('&');
+	var ll = event.latlng;
+	var url = queryLL + ll.lng.toFixed(6) + ',' + ll.lat.toFixed(6);
 
-		loadJSON(url, function(json) {
-			if (outline) { outline.remove(); outline = null; }
-			if (json.results.length === 0) return;
+	function openPopup(json)
+	{
+		removeOutline();
+		if (json.features.length === 0) return;
+		json = json.features[0];
+		var attr = geojson ? json.properties : json.attributes;
+		var agency = attr.Agency;
+		if (agency === 'FS') agency = 'USFS';
 
-			json = json.results[0];
-			var attr = json.attributes;
-			var agency = attr.Agency;
-			if (agency === 'FS') agency = 'USFS';
+		wildernessLink.href = attr.URL;
+		wildernessName.nodeValue = attr.NAME;
+		wildernessYear.nodeValue = '(' + agency + ') (' + attr.YearDesignated + ')';
 
-			wildernessLink.href = attr.URL;
-			wildernessName.nodeValue = attr.NAME;
-			wildernessYear.nodeValue = '(' + agency + ') (' + attr.YearDesignated + ')';
+		map.openPopup(popup.setLatLng(ll));
 
-			map.openPopup(popup.setLatLng(ll));
+		var outlineID = attr[spec.queryFields[0]];
+		outline = outlineCache[outlineID];
+		if (outline) { outline.addTo(map); return; }
 
-			if (json.geometryType !== 'esriGeometryPolygon') return;
-
-			var objectId = attr.OBJECTID_1;
-
-			outline = outlineCache[objectId];
+		function addOutline(json)
+		{
+			if (json.features.length === 0) return;
+			var geometry = json.features[0].geometry;
+			if (!geojson)
+			{
+				if (json.geometryType !== 'esriGeometryPolygon') return;
+				geometry = {type: 'Polygon', coordinates: geometry.rings};
+			}
+			var layer = L.GeoJSON.geometryToLayer(geometry,
+				{color: outlineColor[agency] || '#000000', fillOpacity: 0});
+			outlineCache[outlineID] = layer;
 			if (!outline)
-				outlineCache[objectId] = outline = L.GeoJSON.geometryToLayer(
-					{type: 'Polygon', coordinates: json.geometry.rings},
-					{color: outlineColor[agency] || '#000000', fillOpacity: 0});
+				outline = layer.addTo(map);
+		}
 
-			outline.addTo(map);
-		});
+		loadJSON(queryID + outlineID, addOutline);
+	}
+
+	loadJSON(url, openPopup, removeOutline);
 	});
-}
+};
+
+return MapServer;
+})();
