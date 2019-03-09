109e52107cf14d85039aa8ddd3e1f981d9db9249
Pius Fischer <pius@nightjuggler.com>
2019-03-08 19:21:35 -0800

    Use const instead of var when possible

--- pmap-shared.js
+++ pmap-shared.js
@@ -4,9 +4,9 @@
 var defaultPeakList;
 var topoMaps;
 
-var verticalDatums = [' (NGVD 29)', ' (MSL)', ''];
-var topoSeries = ['7.5', '7.5x15', '15', '30', '60'];
-var topoScale = ['24,000', '25,000', '62,500', '125,000', '250,000'];
+const verticalDatums = [' (NGVD 29)', ' (MSL)', ''];
+const topoSeries = ['7.5', '7.5x15', '15', '30', '60'];
+const topoScale = ['24,000', '25,000', '62,500', '125,000', '250,000'];
 
 function setPopupGlobals(geojson)
 {
@@ -15,7 +15,7 @@ function setPopupGlobals(geojson)
 }
 function fillTopo(f, topoID)
 {
-	let [seriesID, vdatumID, name, year, linkSuffix] = topoMaps[topoID];
+	const [seriesID, vdatumID, name, year, linkSuffix] = topoMaps[topoID];
 
 	f[1] = 'https://ngmdb.usgs.gov/ht-bin/tv_browse.pl?id=' + linkSuffix;
 
@@ -37,14 +37,14 @@ function elevationHTML(elevations)
 	if (typeof elevations === 'string')
 		return elevations;
 
-	var a = [];
-	var f = ['<span><a href="', '', '">', '', '</a><div class="tooltip">', '', '</div></span>'];
+	const a = [];
+	const f = ['<span><a href="', '', '">', '', '</a><div class="tooltip">', '', '</div></span>'];
 
 	for (var e of elevations)
 	{
 		if (typeof e === 'string') { a.push(e); continue; }
 
-		var elevType = e[0];
+		const elevType = e[0];
 
 		if (elevType === 0)
 		{
@@ -58,9 +58,7 @@ function elevationHTML(elevations)
 		}
 		else if (elevType === 2)
 		{
-			let meters = e[1];
-			let stationID = e[2];
-			let stationName = e[3];
+			const [, meters, stationID, stationName] = e;
 
 			f[1] = 'https://www.ngs.noaa.gov/cgi-bin/ds_mark.prl?PidBox=' + stationID;
 			f[3] = Math.round(meters * 39.37 / 12).toLocaleString();
@@ -79,11 +77,11 @@ function elevationHTML(elevations)
 }
 function makePLL(listID, peakID)
 {
-	var url = listID.toLowerCase() + '.html';
-	var txt = listID;
+	let url = listID.toLowerCase() + '.html';
+	let txt = listID;
 
-	var section = peakID.substring(0, peakID.indexOf('.'));
-	var suffix = peakID.charAt(peakID.length - 1);
+	const section = peakID.substring(0, peakID.indexOf('.'));
+	const suffix = peakID.charAt(peakID.length - 1);
 
 	if (suffix === 'd') {
 		url += '?showDelisted';
@@ -101,7 +99,7 @@ function getPLL(peakID) // PLL = Peak List Link(s)
 	if (typeof peakID === 'string')
 		return makePLL(defaultPeakList, peakID);
 
-	var links = [];
+	const links = [];
 
 	for (var id of peakID)
 	{
@@ -129,12 +127,12 @@ function inlineDiv(prefix, content)
 }
 function popupHTML(lng, lat, p)
 {
-	var z = p.z || 15;
-	var b = p.noWX ? 'oo' : 't&o=r&n=0.2';
+	const z = p.z || 15;
+	const b = p.noWX ? 'oo' : 't&o=r&n=0.2';
 
-	var topoLink = 'https://caltopo.com/map.html#ll=' + lat + ',' + lng + '&z=' + z + '&b=' + b;
+	const topoLink = 'https://caltopo.com/map.html#ll=' + lat + ',' + lng + '&z=' + z + '&b=' + b;
 
-	var name = makeLink(topoLink, p.name);
+	let name = makeLink(topoLink, p.name);
 	if (p.HP)
 		name += ' HP';
 	else if (p.emblem)
@@ -144,7 +142,7 @@ function popupHTML(lng, lat, p)
 	if (p.name2)
 		name += '<br>(' + p.name2 + ')';
 
-	var html = makeDiv('peakName', name);
+	let html = makeDiv('peakName', name);
 
 	html += makeDiv('pll', getPLL(p.id));
 	html += makeDiv('peakDiv', inlineDiv('Elevation:', elevationHTML(p.elev)));
@@ -152,7 +150,7 @@ function popupHTML(lng, lat, p)
 	if (p.YDS)
 		html += makeDiv('peakDiv', 'Class ' + p.YDS);
 
-	var links = [];
+	const links = [];
 	if (p.SP) links.push(makeLink('https://www.summitpost.org/' + p.SP, 'SP'));
 	if (p.W) links.push(makeLink('https://en.wikipedia.org/wiki/' + p.W, 'W'));
 	if (p.BB) links.push(makeLink('https://www.snwburd.com/dayhikes/peak/' + p.BB, 'BB'));
