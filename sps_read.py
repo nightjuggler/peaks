3d1d058017458852b70fab4aa27cf0411382b353
Pius Fischer <pius@nightjuggler.com>
2019-11-23 20:12:28 -0800

    Added Isberg Peak, Long Mountain, Mount Ansel Adams, Peak 12573, Donohue Peak, and Mount Andrea Lawrence.

--- sps_read.py
+++ sps_read.py
@@ -37,7 +37,7 @@ peakListsOrdered = [
 	('gbp', 'Great Basin Peaks', 120, 14),
 	('npc', 'Nevada Peaks Club', 76, 6),
 	('odp', 'Other Desert Peaks', 8, 6),
-	('osp', 'Other Sierra Peaks', 70, 27),
+	('osp', 'Other Sierra Peaks', 76, 27),
 	('ocap','Other California Peaks', 84, 14),
 	('owp', 'Other Western Peaks', 8, 10),
 ]
