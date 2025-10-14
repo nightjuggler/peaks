7074992d349261c6664c17472901891863c91600
nightjuggler <pius@nightjuggler.com>
2025-10-14 04:07:04 -0700

    Scripts for working with DEM TIFFs

---
+++ dem/ll2elev.py
@@ -0,0 +1,110 @@
+#!/usr/local/Cellar/rasterio/1.4.3_1/libexec/bin/python
+import os.path
+import re
+import sys
+import numpy as np
+import rasterio
+from rasterio import warp
+from rasterio.crs import CRS
+
+WGS84 = CRS.from_epsg(4326)
+
+def to_meters(feet): return feet * 12/39.37
+def to_feet(meters): return meters * 39.37/12
+
+def get_units(src):
+	units = src.units[0]
+	if units:
+		assert units == 'US survey foot'
+		units, units2, to_units2, wr = 'feet', 'meters', to_meters, 60
+	else:
+		units, units2, to_units2, wr = 'meters', 'feet', to_feet, 20
+	def elev_str(elev):
+		return f'{elev:.1f} {units} = {to_units2(elev):.1f} {units2}'
+
+	return units, elev_str, wr
+
+def process(src, lat, lng):
+	in_lat, lat_digits = lat
+	in_lng, lng_digits = lng
+	lat = in_lat
+	lng = in_lng
+
+	units, elev_str, wr = get_units(src)
+	ww = 2*wr + 1
+
+	(x,), (y,) = warp.transform(WGS84, src.crs, [lng], [lat])
+	row, col = src.index(x, y)
+	x2, y2 = src.transform * (col, row)
+#	assert row, col == src.index(x2, y2)
+	w = rasterio.windows.Window(col-wr, row-wr, ww, ww)
+	elevs = src.read(indexes=1, window=w)
+	elev = elevs[wr][wr]
+
+	print('\nElevation at the converted input latitude/longitude:')
+	print(f'{lng:.6f} {lat:.6f} => {x:.3f} {y:.3f} => {col} {row} => {x2:.3f} {y2:.3f}')
+	print(elev_str(elev))
+
+	cols = np.tile(range(col-wr, col+wr+1), ww) # 0,1,2,0,1,2,0,1,2
+	rows = np.repeat(range(row-wr, row+wr+1), ww) # 0,0,0,1,1,1,2,2,2
+	xs, ys = src.transform * (cols, rows)
+	lngs, lats = warp.transform(src.crs, WGS84, xs, ys)
+	elevs = sorted(zip(elevs.flat, lats, lngs, ys, xs, rows, cols, strict=True))
+	elev, lat, lng, y, x, row, col = elevs[-1]
+
+	print(f'\nMaximum elevation within {wr} {units}:')
+	print(f'{col} {row} => {x:.3f} {y:.3f} => {lng:.6f} {lat:.6f}')
+	print(elev_str(elev))
+
+	print('\nLocations that round to the input latitude/longitude:')
+	p_lat = lat_digits + 2
+	p_lng = lng_digits + 2
+	for elev, lat, lng, y,  x, row, col in elevs:
+		if round(lat, lat_digits) == in_lat and round(lng, lng_digits) == in_lng:
+			print(f'{col} {row} | {x:.1f} {y:.1f} | '
+				f'{lng:.{p_lng}f} {lat:.{p_lat}f} |', elev_str(elev))
+
+def get_filenames(lat, lng):
+	lat = lat[0]
+	lng = lng[0]
+	project = 'CA_SantaClaraCounty_2020_A20'
+	filenames = []
+
+	(x,), (y,) = warp.transform(WGS84, CRS.from_epsg(6420), [lng], [lat])
+	x = int(round(x)) // 2500 * 25 % 10_000
+	y = int(round(y)) // 2500 * 25 % 10_000
+	filenames.append(f'USGS_OPR_{project}_{x:04}{y:04}.tif')
+
+	(x,), (y,) = warp.transform(WGS84, CRS.from_epsg(26910), [lng], [lat])
+	x = int(round(x)) // 10_000
+	y = int(round(y)) // 10_000 + 1
+	filenames.append(f'USGS_1M_10_x{x}y{y}_{project}.tif')
+
+	return filenames
+
+ll_pattern = re.compile('-?[0-9]{1,3}(\\.[0-9]{1,14})?')
+
+def get_ll(arg):
+	m = ll_pattern.fullmatch(arg)
+	return (float(arg), len(m.group(1))-1) if m else None
+
+def main(args):
+	if len(args) != 4:
+		return f'Usage: {args[0]} <latitude> <longitude> <directory>'
+
+	lat, lng, path = args[1:]
+	if not (lat := get_ll(lat)):
+		return 'Please specify a valid latitude!'
+	if not (lng := get_ll(lng)):
+		return 'Please specify a valid longitude!'
+
+	for filename in get_filenames(lat, lng):
+		filename = os.path.join(path, filename)
+		if os.path.exists(filename):
+			print('Opening', filename)
+			with rasterio.open(filename) as src:
+				process(src, lat, lng)
+	print()
+
+if __name__ == '__main__':
+	sys.exit(main(sys.argv))
---
+++ dem/ll2tif.zsh
@@ -0,0 +1,61 @@
+#
+# The USGS_OPR_CA_SantaClaraCounty_2020_A20_{x}{y}.tif tiles at
+# https://prd-tnm.s3.amazonaws.com/index.html?prefix=StagedProducts/Elevation/OPR/Projects/CA_SantaClaraCounty_2020_A20/CA_SantaClaraCounty_2020/
+# use EPSG 6420 (https://epsg.io/6420) (NAD83(2011) / California zone 3 (ftUS))
+# OPR = Original Product Resolution
+# These tiles are also available Deflate-compressed at
+# https://noaa-nos-coastal-lidar-pds.s3.us-east-1.amazonaws.com/dem/CA_Santa_Clara_DEM_2020_9330/index.html
+#
+# x ranges from 0525 to 3675 in increments of 25
+# y ranges from 7700 to 0150 in increments of 25
+#
+# The x and y correspond to the bottom-left corner of the tile.
+# int(round(src.bounds.left,7)) == (x + 60_000) * 100
+# int(round(src.bounds.bottom,7)) == (y + (20_000 if y < 1_000 else 10_000)) * 100
+#
+# The data in these tiles is a 2500 x 2500 float32 array of elevations in feet.
+# int(round(src.bounds.left,7)) + 2500 == int(round(src.bounds.right,7))
+# int(round(src.bounds.bottom,7)) + 2500 == int(round(src.bounds.top,7))
+#
+# To determine the filename of the tile that contains a given lat/lng, convert the lat/lng
+# to EPSG 6420 easting (x) and northing (y) and take digits 2 through 5 of each coordinate.
+#
+proj=/usr/local/bin/proj
+project='CA_SantaClaraCounty_2020_A20'
+
+ll2tif_opr() {
+	if (( $# != 2 )) { return }
+	xy=(`echo $2 $1 | $proj EPSG:6420 -f '%.0f'`)
+	(( x = xy[1] / 2500 * 25 % 10000 ))
+	(( y = xy[2] / 2500 * 25 % 10000 ))
+	print -n $xy ''
+	print -f 'USGS_OPR_%s_%04d%04d.tif\n' $project $x $y
+}
+#
+# The USGS_1M_10_x[x]y[y]_CA_SantaClaraCounty_2020_A20.tif tiles at
+# https://prd-tnm.s3.amazonaws.com/index.html?prefix=StagedProducts/Elevation/1m/Projects/CA_SantaClaraCounty_2020_A20/
+# use EPSG 26910 (https://epsg.io/26910) (NAD83 / UTM zone 10N)
+#
+# x ranges from 56 to 66 in increments of 1
+# y ranges from 408 to 416 in increments of 1
+#
+# The x and y correspond to the top-left corner of the tile.
+# int(round(src.bounds.left,4)) == x*10_000 - 6
+# int(round(src.bounds.top,4)) == y*10_000 + 6
+#
+# The data in these tiles is a 10012 x 10012 float32 array of elevations in meters.
+# int(round(src.bounds.left,4)) + 10_012 == int(round(src.bounds.right,4))
+# int(round(src.bounds.top,4)) - 10_012 == int(round(src.bounds.bottom,4))
+#
+ll2tif_1m() {
+	if (( $# != 2 )) { return }
+	xy=(`echo $2 $1 | $proj EPSG:26910 -f '%.0f'`)
+	(( x = xy[1] / 10000 ))
+	(( y = xy[2] / 10000 + 1 ))
+	print -n $xy ''
+	print -f 'USGS_1M_10_x%dy%d_%s.tif\n' $x $y $project
+}
+ll2tif_test() {
+	ll2tif_opr 37.21723 -121.44024 # x=6287881 y=1902943 (ft)
+	ll2tif_1m 37.21723 -121.44024 # x=638390 y=4120110 (m)
+}
