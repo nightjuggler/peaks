cc4405ccb8734455bbc5ac5a0f1ad46e13ea009f
nightjuggler <pius@nightjuggler.com>
2026-08-30 17:34:25 -0700

    Add Lovers Leap

--- sps_read.py
+++ sps_read.py
@@ -44,7 +44,7 @@ peakListsOrdered = [
 	('gbp', 'Great Basin Peaks', 120, 14),
 	('npc', 'Nevada Peaks Club', 76, 6),
 	('odp', 'Other Desert Peaks', 10, 6),
-	('osp', 'Other Sierra Peaks', 100, 27),
+	('osp', 'Other Sierra Peaks', 101, 27),
 	('ocap','Other California Peaks', 124, 17),
 	('owp', 'Other Western Peaks', 21, 10),
 ]
